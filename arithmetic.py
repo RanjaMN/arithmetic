9e998770f9fd2b4f429e3b565f585ecebdb14950
RanjaMN <82496123+RanjaMN@users.noreply.github.com>
2021-04-14 12:19:55 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the Number A"))
+b=int(input("Enter the Number B"))
+print("Addition=",a+b)
+print("Subraction=",a-b)
+print("Multiplication=",a*b)
+print("Division=",a/b)     
+      
